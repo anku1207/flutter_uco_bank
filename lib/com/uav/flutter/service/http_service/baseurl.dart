2f6f8170905fca7bc000aa3ca8e0e900c44572fd
Ankit Roy <chemicalexceptions@gmail.com>
2021-07-16 16:23:03 +0530

    add

--- lib/com/uav/flutter/service/http_service/baseurl.dart
+++ lib/com/uav/flutter/service/http_service/baseurl.dart
@@ -2,6 +2,6 @@ class ApiUrl {
 // const val BASE_URL = "http://192.168.3.121:8986/api/" // base url
 //const val BASE_URL = "http://192.168.1.7:8001/api/" // base url
 //const val BASE_URL = "http://192.168.43.185:8001/api/" // base url
-static const BASE_URL = "http://cbsl_connect.cbslgroup.in/api/"; // base url
-
+//static const BASE_URL = "http://cbsl_connect.cbslgroup.in/api/"; // base url
+  static const BASE_URL = "http://uco-pms.cbslgroup.in/api/";
 }
